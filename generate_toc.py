65d0a066277dc2febb5e93721e196ba238eaec1f
karenyyng <karen.yyng@gmail.com>
2019-06-05 17:21:49 -0700

    Add script for adding table-of-content

---
+++ generate_toc.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+generate table-of-content (toc) for each of the markdown notes
+"""
+import os
+import subprocess
+import copy
+
+if __name__ == "__main__":
+    topdir = '.'
+    # The extension to search for
+    exten = '.md'
+
+    lines = "# Table of content\n<!-- toc -->\n<!-- tocstop -->\n\n"
+    toc_start = "<!-- toc -->"
+
+    for dirpath, dirnames, files in os.walk(topdir):
+        for name in files:
+            if name.endswith(exten):
+                file_path = f"{dirpath}/{name}"
+                print(f"!!!! ---Processing {file_path} --- !!!")
+                with open(file_path) as f:
+                    file_content = "".join(f.readlines())
+
+                if toc_start not in file_content:
+                    print(f"!!!! ---TOC mark {toc_start} not present -!!!")
+                    file_content = lines + file_content
+                    with open(file_path, "w") as f:
+                        print('!!!! ---Adding TOC mark to file content -!!!')
+                        # print(file_content)
+                        f.writelines(file_content)
+
+                print(f"!!!! --- Generating TOC for {file_path} - !!!!")
+                subprocess.call(
+                        [f"/usr/local/bin/markdown-toc -i {file_path}"],
+                        shell=True)
